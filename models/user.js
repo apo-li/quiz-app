d81915112e98ca334517c4cafd95564f1d2b2de2
apo-li <apistolos@yahoo.gr>
2025-04-10 18:27:25 +0300

    small changes

--- models/user.js
+++ models/user.js
@@ -23,10 +23,9 @@ const userSchema = new mongoose.Schema({
     
 })
 
-// module.exports = mongoose.model('User', userSchema)
+// module.exports = mongoose.model('User', userSchema)   //gia ylopoihsh xwris classes
 
-
-////////////////////////////////////////////////////////////////////////// 
+//////////////////////////////////////////////
 
 ////      ylopoihsh me oop (classes)      ////
 

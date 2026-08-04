844afcd32623f4d9a0ae30fe4c4f96d0b59097b9
apo-li <apistolos@yahoo.gr>
2026-08-04 23:54:18 +0300

    cleaned old v1 files

--- routes/create.js
+++ routes/create.js
@@ -2,12 +2,6 @@ const express = require('express')
 const router = express.Router()
 const createController = require('../controllers/create')
 
-// router.get('/create', createController.showCreate)
-// router.post('/create', createController.create)
-
-// router.get('/create/add-questions/:quizId', createController.showAddQuestions)
-// router.post('/create/add-questions/:quizId', createController.addQuestions)
-
 router.get('/create-quiz', createController.showCreate)
 router.post('/create-quiz', createController.create)
 
